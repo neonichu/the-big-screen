359f98fd4fe49e3f1d50941c728ac0c826ee20c3
Boris Bügling <boris@icculus.org>
2015-10-30 07:41:41 -0700

    Focus: add custom view

---
+++ examples/Focus/Focus/CustomFocusView.swift
@@ -0,0 +1,27 @@
+//
+//  CustomFocusView.swift
+//  Focus
+//
+//  Created by Boris Bügling on 30/10/15.
+//  Copyright © 2015 Boris Bügling. All rights reserved.
+//
+
+import UIKit
+
+class CustomFocusView: UIView {
+    override func canBecomeFocused() -> Bool {
+        return true
+    }
+
+    override func didUpdateFocusInContext(context: UIFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator) {
+        coordinator.addCoordinatedAnimations({
+            if context.nextFocusedView == self {
+                self.backgroundColor = UIColor.redColor()
+            }
+
+            if context.previouslyFocusedView == self {
+                self.backgroundColor = UIColor.whiteColor()
+            }
+        }, completion: nil)
+    }
+}
