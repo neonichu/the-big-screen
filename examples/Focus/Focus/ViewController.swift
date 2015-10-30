aa188149bcc9c485289cac70f284965c18b4db76
Boris Bügling <boris@icculus.org>
2015-10-30 07:34:06 -0700

    Focus: initial commit

---
+++ examples/Focus/Focus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Focus
+//
+//  Created by Boris Bügling on 30/10/15.
+//  Copyright © 2015 Boris Bügling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
